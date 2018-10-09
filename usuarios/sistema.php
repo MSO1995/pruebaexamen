bcde4a7bf060530ac646946d795afddcbafe5930
María del Socorro Ortiz González <43865201+MSO1995@users.noreply.github.com>
2018-10-09 09:02:46 -0500

    Update sistema.php

--- usuarios/sistema.php
+++ usuarios/sistema.php
@@ -2,15 +2,21 @@
  session_start();
 
  if (!empty($_SESSION['id']) && !empty($_SESSION['pass'])) {
-  
- require_once 'LIGA3/LIGA.php'; 
-  
+  require_once 'LIGA3/LIGA.php'; 
+  BD('localhost', 'root', '', 'base');
   HTML::cabeceras(array('title'=>'Sistema seguro', 'description'=>'Lo que sea...'));
+  //echo 'usuario admitido';
   
+   $body = array('contenedor'=>array('uno'=>'<p>Usuario válido</p>',
+                                    'dos'=>'<a href="cerrar.php">Cerrar sesión</a>'
+                                    ));
   
+  $usuario=LIGA('usuarios');
+  $columna=('id,nombre,fecha');
+  $ppt=array( 'Usuario' => '@[nombre]',
+             'Hora de registro' => '@{substr("@[fecha]",11,19)}@');
   
-  $body = array('contenedor'=>array('uno'=>'<p>Usuario válido</p>',
-                                    'dos'=>'<a href="cerrar.php">Cerrar sesión</a>'));
+  HTML::tabla($usuario, 'USUARIO REGISTRADO',$ppt);
   
   HTML::cuerpo($body);
   
@@ -19,3 +25,4 @@
     //echo 'Área prohibida';
     header('Location: .?error=1');
  }
+
